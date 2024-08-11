0fe12cdea3f38dbbf62d9364935b1aa6d3352ac4
beomshin.kim <chzh859@naver.com>
2024-08-11 14:38:05 +0900

    # 동일 페이지 이동 prevent
    - context path 타임리프 설정

--- src/main/resources/static/js/common/header.js
+++ src/main/resources/static/js/common/header.js
@@ -14,16 +14,20 @@ document.getElementById('sideBarClose').addEventListener('click', function () {
 })
 
 // 알림창 클릭 드롭 다운
-document.getElementById('alert').addEventListener('click', function () {
-    dropDownOff('header-myinfo-dropdown')
-    dropDownToggle('header-message-dropdown')
-})
+if (document.getElementById('alert')) {
+    document.getElementById('alert').addEventListener('click', function () {
+        dropDownOff('header-myinfo-dropdown')
+        dropDownToggle('header-message-dropdown')
+    })
+}
 
 // 나의 프로필 클릭 드롭 다운
-document.getElementById('myInfo').addEventListener('click', function () {
-    dropDownOff('header-message-dropdown')
-    dropDownToggle('header-myinfo-dropdown')
-})
+if (document.getElementById("myInfo")) {
+    document.getElementById('myInfo').addEventListener('click', function () {
+        dropDownOff('header-message-dropdown')
+        dropDownToggle('header-myinfo-dropdown')
+    })
+}
 
 function dropDownToggle(className) {
     const dropdown = document.getElementsByClassName(className)[0];
@@ -80,6 +84,19 @@ function lck() {
 }
 
 function move(path) {
-    const pathname = "/" + window.location.pathname.split("/")[1];
-    window.location.href = pathname + path;
+    const contextPath = $('#contextPathHolder').attr('data-contextPath');// 현재 URL이 '/'인지 확인하는 스크립트
+    window.location.href = contextPath + path;
 }
+
+$(document).ready(function () {
+    document.querySelectorAll('.header a').forEach(function (link) {
+        // 링크의 href 경로 가져오기
+        var linkPath = link.getAttribute('href');
+
+        if (linkPath === window.location.pathname) { // 현재 경로와 링크 경로가 동일하면 클릭 이벤트를 막음
+            link.addEventListener('click', function (event) {
+                event.preventDefault();  // 링크 동작을 차단
+            });
+        }
+    });
+})
